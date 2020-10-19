bca0fd79ed8951c65cf42bdc31b5e24ee246da23
Silas Beckmann <silas.beckmann@dreid.de>
2020-10-19 07:55:48 +0200

    WIP

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+         'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -35,8 +36,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Passport::routes(null, ['middleware' => [ \Fruitcake\Cors\HandleCors::class ]]);
-
-        Passport::tokensExpireIn(now()->addDay());
+        Route::group([ ‘middleware’ => ‘cors’], function() {
+            Passport::routes();
+        });
     }
 }
