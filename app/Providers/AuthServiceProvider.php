7c7502aa86387498df9758114b276576bd7edcdc
Silas Beckmann <silas.beckmann@dreid.de>
2020-10-19 07:56:25 +0200

    WIP

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-         'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -36,8 +36,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Route::group([ ‘middleware’ => ‘cors’], function() {
-            Passport::routes();
-        });
+        Passport::routes();
     }
 }
