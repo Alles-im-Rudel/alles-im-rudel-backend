e1a6e3fec145c3c2523a9ab508313a0af2a105ed
Silas <silas.beckmann@t-online.de>
2021-08-01 18:35:50 +0200

    ImportRiotVersionsCommand => daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(ImportRiotVersionsCommand::class)->weeklyOn(3);
+        $schedule->command(ImportRiotVersionsCommand::class)->daily();
     }
 
     /**
