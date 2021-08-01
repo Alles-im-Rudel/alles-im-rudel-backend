d65b0353cc6b7f1c84c652a39d9f75878347c236
vollborn <oliver.vollborn@dreid.de>
2021-08-01 16:44:41 +0200

    UserGroups Seeder

--- app/Models/UserGroup.php
+++ app/Models/UserGroup.php
@@ -10,19 +10,41 @@ use Spatie\Permission\Traits\HasRoles;
 
 class UserGroup extends Model
 {
-	use SoftDeletes, HasRoles, BelongsToLevel;
+	use SoftDeletes,
+        HasRoles,
+        BelongsToLevel;
 
-	protected string $guard_name = 'web';
-
+	/*
+	 * Admins
+	 */
 	public const DEVELOPER_ID = 1;
-	public const ADMIN = 2;
-	public const MODERATOR = 3;
-	public const MEMBER = 4;
-	public const E_SPORTS = 5;
-	public const AIRSOFT = 6;
-	public const PROSPECT = 7;
-	public const GUEST = 8;
-	public const NEW = 11;
+	public const ADMIN_ID = 2;
+
+	/*
+	 * Moderators
+	 */
+	public const MODERATOR_ID = 3;
+	public const AIRSOFT_LEADER_ID = 4;
+	public const E_SPORTS_LEADER_ID = 5;
+
+	/*
+	 * Members
+	 */
+	public const MEMBER_ID = 6;
+	public const E_SPORTS_MEMBER_ID = 7;
+	public const AIRSOFT_MEMBER_ID = 8;
+
+	/*
+	 * Prospect
+	 */
+	public const AIRSOFT_PROSPECT_ID = 9;
+	public const E_SPORTS_PROSPECT_ID = 12;
+
+	/*
+	 * Guests
+	 */
+    public const GUEST_ID = 10;
+	public const FRIEND_ID = 13;
 
 	protected $fillable = [
 		'display_name',
--- database/seeders/UserGroupsSeeder.php
+++ database/seeders/UserGroupsSeeder.php
@@ -10,104 +10,114 @@ use Illuminate\Database\Seeder;
 
 class UserGroupsSeeder extends Seeder
 {
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run(): void
-	{
-		$userGroups = $this->getUserGroups();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        foreach ($this->getUserGroups() as $userGroup) {
+            UserGroup::updateOrCreate([
+                'id' => $userGroup['id']
+            ], [
+                'level_id'     => $userGroup['level_id'],
+                'display_name' => $userGroup['display_name'],
+                'description'  => $userGroup['description'],
+                'color'        => $userGroup['color'],
+            ]);
+        }
 
-		foreach ($userGroups as $userGroup) {
-			UserGroup::firstOrCreate([
-				'display_name' => $userGroup['display_name']
-			], [
-				'level_id'    => $userGroup['level_id'],
-				'description' => $userGroup['description'],
-				'color'       => $userGroup['color'],
-			]);
-		}
+        UserGroup::find(UserGroup::DEVELOPER_ID)->syncPermissions(Permission::all());
+    }
 
-		UserGroup::find(UserGroup::DEVELOPER_ID)->syncPermissions(Permission::all());
-	}
-
-	public function getUserGroups(): array
-	{
-		return [
-			[
-				'level_id'     => Level::DEVELOPER,
-				'display_name' => 'Developer',
-				'color'        => 'dev',
-				'description'  => 'Offizieller Developer des Vereins Alles im Rudel.',
-			],
-			[
-				'level_id'     => Level::ADMINISTRATOR,
-				'display_name' => 'Vorstand',
-				'color'        => 'admin',
-				'description'  => 'Offizieller Vorstand des Vereins Alles im Rudel.',
-			],
-			[
-				'level_id'     => Level::MODERATOR,
-				'display_name' => 'Moderator',
-				'color'        => 'moderator',
-				'description'  => 'Offizieller Moderator des Vereins Alles im Rudel.',
-			],
-			[
-				'level_id'     => Level::MODERATOR,
-				'display_name' => 'Airsoft Leiter',
-				'color'        => 'moderator',
-				'description'  => 'Offizieller Leiter der Sparte Airsoft des Vereins Alles im Rudel.',
-			],
-			[
-				'level_id'     => Level::MODERATOR,
-				'display_name' => 'E-Sports Leiter',
-				'color'        => 'moderator',
-				'description'  => 'Offizieller Leiter der Sparte E-Sports des Vereins Alles im Rudel.',
-			],
-			[
-				'level_id'     => Level::MEMBER,
-				'display_name' => 'Vereinsmitglied',
-				'color'        => 'member',
-				'description'  => 'Offizielles Mitglied des Vereins Alles im Rudel.',
-			],
-			[
-				'level_id'     => Level::MEMBER,
-				'display_name' => 'E-Sports Mitglied',
-				'color'        => 'eSports',
-				'description'  => 'Offizielles Mitglied der Sparte E-Sports des Vereins Alles im Rudel.',
-			],
-			[
-				'level_id'     => Level::MEMBER,
-				'display_name' => 'Airsoft Mitglied',
-				'color'        => 'airsoft',
-				'description'  => 'Offizielles Mitglied der Sparte Airsoft des Vereins Alles im Rudel.',
-			],
+    public function getUserGroups(): array
+    {
+        return [
             [
+                'id'           => UserGroup::DEVELOPER_ID,
+                'level_id'     => Level::DEVELOPER,
+                'display_name' => 'Developer',
+                'color'        => 'dev',
+                'description'  => 'Offizieller Developer des Vereins Alles im Rudel.',
+            ],
+            [
+                'id'           => UserGroup::DEVELOPER_ID,
+                'level_id'     => Level::ADMINISTRATOR,
+                'display_name' => 'Vorstand',
+                'color'        => 'admin',
+                'description'  => 'Offizieller Vorstand des Vereins Alles im Rudel.',
+            ],
+            [
+                'id'           => UserGroup::MODERATOR_ID,
+                'level_id'     => Level::MODERATOR,
+                'display_name' => 'Moderator',
+                'color'        => 'moderator',
+                'description'  => 'Offizieller Moderator des Vereins Alles im Rudel.',
+            ],
+            [
+                'id'           => UserGroup::AIRSOFT_LEADER_ID,
+                'level_id'     => Level::MODERATOR,
+                'display_name' => 'Airsoft Leiter',
+                'color'        => 'moderator',
+                'description'  => 'Offizieller Leiter der Sparte Airsoft des Vereins Alles im Rudel.',
+            ],
+            [
+                'id'           => UserGroup::E_SPORTS_LEADER_ID,
+                'level_id'     => Level::MODERATOR,
+                'display_name' => 'E-Sports Leiter',
+                'color'        => 'moderator',
+                'description'  => 'Offizieller Leiter der Sparte E-Sports des Vereins Alles im Rudel.',
+            ],
+            [
+                'id'           => UserGroup::MEMBER_ID,
+                'level_id'     => Level::MEMBER,
+                'display_name' => 'Vereinsmitglied',
+                'color'        => 'member',
+                'description'  => 'Offizielles Mitglied des Vereins Alles im Rudel.',
+            ],
+            [
+                'id'           => UserGroup::E_SPORTS_MEMBER_ID,
+                'level_id'     => Level::MEMBER,
+                'display_name' => 'E-Sports Mitglied',
+                'color'        => 'eSports',
+                'description'  => 'Offizielles Mitglied der Sparte E-Sports des Vereins Alles im Rudel.',
+            ],
+            [
+                'id'           => UserGroup::AIRSOFT_MEMBER_ID,
+                'level_id'     => Level::MEMBER,
+                'display_name' => 'Airsoft Mitglied',
+                'color'        => 'airsoft',
+                'description'  => 'Offizielles Mitglied der Sparte Airsoft des Vereins Alles im Rudel.',
+            ],
+            [
+                'id'           => UserGroup::AIRSOFT_PROSPECT_ID,
                 'level_id'     => Level::PROSPECT,
                 'display_name' => 'Airsoft Anwärter',
                 'color'        => 'prospect',
                 'description'  => 'Offizieller Anwärter der Sparte Airsoft des Vereins Alles im Rudel.',
             ],
             [
+                'id'           => UserGroup::E_SPORTS_PROSPECT_ID,
                 'level_id'     => Level::PROSPECT,
                 'display_name' => 'E-Sports Anwärter',
                 'color'        => 'prospect',
                 'description'  => 'Offizieller Anwärter der Sparte E-Sports des Vereins Alles im Rudel.',
             ],
             [
+                'id'           => UserGroup::FRIEND_ID,
                 'level_id'     => Level::GUEST,
                 'display_name' => 'Freund',
                 'color'        => 'guest',
                 'description'  => 'Freund.',
             ],
             [
+                'id'           => UserGroup::GUEST_ID,
                 'level_id'     => Level::GUEST,
                 'display_name' => 'Gast',
                 'color'        => 'guest',
                 'description'  => 'Gast.',
             ]
-		];
-	}
-
+        ];
+    }
 }
